7b6068a0559eb34da43ea55a865486b1369c2bde
KristinBethea <112720229+KristinBethea@users.noreply.github.com>
2023-12-08 16:11:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,62 @@
-// create web server
-const http = require('http');
+// Create web server
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+var http = require('http');
+
+// connect to database
+const db = require('../database/index.js');
+
+// middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// routes
+app.get('/comments', (req, res) => {
+  db.getComments((err, data) => {
+    if (err) {
+      console.log('error retrieving comments');
+      res.status(404).send();
+    } else {
+      console.log('retrieved comments');
+      res.status(200).send(data);
+    }
+  });
 });
 
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Server running at http://127.0.0.1:3000/');
+app.post('/comments', (req, res) => {
+  db.postComment(req.body, (err) => {
+    if (err) {
+      console.log('error posting comment');
+      res.status(404).send();
+    } else {
+      console.log('posted comment');
+      res.status(201).send();
+    }
+  });
 });
+
+app.delete('/comments', (req, res) => {
+  db.deleteComment(req.body, (err) => {
+    if (err) {
+      console.log('error deleting comment');
+      res.status(404).send();
+    } else {
+      console.log('deleted comment');
+      res.status(204).send();
+    }
+  });
+});
+
+// create a webserver using http
+var server  = http.createServer(app);
+server.listen(PORT);
+
+// app listen also rises a problem
+// app.listen(PORT, () => console.log(`listening on port ${PORT}`));
